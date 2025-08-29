06b49c442ba2564eac604522dcad15973139ab03
guiln <guilherme.l.nunes@gmail.com>
2025-08-29 01:51:07 -0300

    add lib requirements

--- utils/setup.py
+++ utils/setup.py
@@ -6,6 +6,10 @@ setup(
     description="Utility modules for the monorepo, including logging and more.",
     author="Your Name",
     packages=find_packages(),
-    install_requires=[],  # Add dependencies here if needed
+    install_requires=[
+        "certifi==2025.8.3",
+        "sentry-sdk==2.35.1",
+        "urllib3==2.5.0",
+    ],
     python_requires=">=3.6",
 )
