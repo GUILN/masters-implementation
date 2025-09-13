3a4961fd1fb1696050e162593148cc69796c44ec
guiln <guilherme.l.nunes@gmail.com>
2025-09-12 22:05:47 -0300

    bump version

--- utils/setup.py
+++ utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="utils",
-    version="0.1.0",
+    version="0.2.0",
     description="Utility modules for the monorepo, including logging and more.",
     author="Your Name",
     packages=find_packages(),
