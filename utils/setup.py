ec71dbd5a842ad35c08e82e6f7b9d24900a21f43
guiln <guilherme.l.nunes@gmail.com>
2025-09-12 22:15:03 -0300

    downgrade urlib for backard compatibility

--- utils/setup.py
+++ utils/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="utils",
-    version="0.2.0",
+    version="1.0.0",
     description="Utility modules for the monorepo, including logging and more.",
     author="Your Name",
     packages=find_packages(),
     install_requires=[
         "certifi==2025.8.3",
         "sentry-sdk==2.35.1",
-        "urllib3==2.5.0",
+        "urllib3>=1.26.0",
     ],
     python_requires=">=3.6",
 )
