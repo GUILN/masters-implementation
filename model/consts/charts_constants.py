fb849fa687eda47a4ed69f6a2b5a603a9fc80d33
guinunestw <gui.nunes@onepeloton.com>
2025-12-12 13:45:53 -0300

    translate / use charts constants class

--- model/consts/charts_constants.py
+++ model/consts/charts_constants.py
@@ -4,15 +4,20 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class ChartsLabels:
+    # General labels
     with_object_branch: str
     without_object_branch: str
     confusion_matrix_true: str
     confusion_matrix_predicted: str
+    # Quantitative labels
     quantitative_observation_rate: str
     quantitative_auc: str
     quantitative_auc_progression_over_observation_rate: str
     quantitative_accuracy: str
     quantitative_accuracy_progression_over_observation_rate: str
+    # Qualitative labels
+    t_sne: str
+    t_sne_visualization: str
 
 
 portuguese_labels = ChartsLabels(
@@ -25,6 +30,8 @@ portuguese_labels = ChartsLabels(
     quantitative_auc_progression_over_observation_rate="Progressão do AUC com a Taxa de Observação",
     quantitative_accuracy="Acurácia",
     quantitative_accuracy_progression_over_observation_rate="Progressão da Acurácia com a Taxa de Observação",
+    t_sne="t-SNE",
+    t_sne_visualization="Visualização t-SNE",
 )
 
 
